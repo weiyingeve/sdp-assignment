b32ed9b28929bcdda6754d94762045d00a3af570
Klaris <s10257959@connect.np.edu.sg>
2025-02-15 08:15:20 +0800

    added filter by type and state

---
+++ sdp-assignment/StateDocumentIterator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sdp_assignment
+{
+    public class StateDocumentIterator : IDocumentIterator
+    {
+        private List<Document> documents;
+        private string state;
+        private int position = 0;
+
+        public StateDocumentIterator(List<Document> documents, string state)
+        {
+            this.documents = documents;
+            this.state = state;
+        }
+
+        public bool HasNext()
+        {
+            while (position < documents.Count)
+            {
+                if (documents[position].getState().GetType().Name.Equals(state, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+                position++;
+            }
+            return false;
+        }
+
+        public Document Next()
+        {
+            return documents[position++];
+        }
+    }
+
+}
---
+++ sdp-assignment/TypeDocumentIterator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sdp_assignment
+{
+    public class TypeDocumentIterator : IDocumentIterator
+    {
+        private List<Document> documents;
+        private string docType;
+        private int position = 0;
+
+        public TypeDocumentIterator(List<Document> documents, string docType)
+        {
+            this.documents = documents;
+            this.docType = docType;
+        }
+
+        public bool HasNext()
+        {
+            while (position < documents.Count)
+            {
+                if (documents[position].GetType().Name.Equals(docType, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+                position++;
+            }
+            return false;
+        }
+
+        public Document Next()
+        {
+            return documents[position++];
+        }
+    }
+
+}
